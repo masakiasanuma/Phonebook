14892508e8658e03585103bcfee0a63f9856e5f5
Masaki <masakiasanuma@outlook.com>
2020-04-10 18:20:46 -0700

    Initial commit, adding current progress

---
+++ build/precache-manifest.0da5cf4cc6f8c2fa89b45360e01344aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b824901ebae228e82cc6bc5fdc37b22d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8c1ec452134d3c62ce3",
+    "url": "/static/css/main.c3da8881.chunk.css"
+  },
+  {
+    "revision": "893703fe2faddbd19b24",
+    "url": "/static/js/2.79f71ccb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.79f71ccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8c1ec452134d3c62ce3",
+    "url": "/static/js/main.cd0603a4.chunk.js"
+  },
+  {
+    "revision": "b88786010dfc72db4710",
+    "url": "/static/js/runtime-main.ebb9559b.js"
+  }
+]);
